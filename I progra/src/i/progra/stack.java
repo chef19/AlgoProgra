8035aae6e556e24d0381eb66ec40dc3cbabefa6a
Ricardo Araya <Ricardo Araya@Ricardo.tigo.co.cr>
2015-03-19 02:06:55 -0600

    Elaboracion de Stack

--- I progra/src/i/progra/stack.java
+++ I progra/src/i/progra/stack.java
@@ -7,8 +7,61 @@ package i.progra;
 
 /**
  *
- * @author Kevin Matamoros
+ * @author Ricardo Araya
  */
 public class stack {
+    public Nodo top;
+    public int size;
+    
+    
+    
+    public stack(){
+        this.top=null;
+        this.size=0;
+    }
+    
+    public void push(Object elemento){
+        top=new Nodo(elemento,top);
+        size++;
+        System.out.println(elemento);
+    }
+    
+    public void pop(){
+        if(top==null || size==0){
+            System.out.println("Esta vacia");
+        }
+        size--;
+        System.out.println(top);
+    }
+    
+    
+    
+    
+    public static void main(String args[]){
+        stack a=new stack();
+        a.push(335);
+        a.push(336);
+        a.push(337);
+        a.pop();
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
